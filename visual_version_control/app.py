35ac60a65d2d30f9fa0fb796f99f12368618f3a7
RedPillCoder <Tappilina@tutanota.com>
2025-02-22 15:18:24 +0800

    Update app.py

--- visual_version_control/app.py
+++ visual_version_control/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, jsonify
-import os
+from flask import Flask, render_template, jsonify, request
 import json
 
 app = Flask(__name__)
@@ -16,8 +15,12 @@ version_data = [
 def index():
     return render_template('index.html')
 
-@app.route('/api/versions')
-def get_versions():
+@app.route('/api/versions', methods=['GET', 'POST'])
+def manage_versions():
+    if request.method == 'POST':
+        new_version = request.json
+        version_data.append(new_version)
+        return jsonify(new_version), 201
     return jsonify(version_data)
 
 if __name__ == '__main__':
